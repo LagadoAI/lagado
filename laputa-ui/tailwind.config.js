f69cb03b420d4f224fb18d6eb0ec032d8c99ab46
Lagado Labs <lagadolabs@gmail.com>
2026-05-30 22:17:30 -0500

    ui: shadcn init + css fixes

--- laputa-ui/tailwind.config.js
+++ laputa-ui/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      borderColor: {
+        border: 'hsl(var(--border))',
+      },
       colors: {
         laputa: {
           bg: "#0f1117",
